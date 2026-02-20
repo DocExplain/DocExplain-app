851a82f799ee78408066ddf5178f72cd1c79be77
tuorn <nicolas.tuor@unifr.ch>
2026-02-20 15:34:55 +0100

    update android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.docexplain.documate',
+  appId: 'com.documate.app',
   appName: 'DocuMate AI',
   webDir: 'dist',
   plugins: {

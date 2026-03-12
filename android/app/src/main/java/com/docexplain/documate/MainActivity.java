4ac9dca3817f3d4cbd6639025198c71b1490c9f0
tuorn <nicolas.tuor@unifr.ch>
2026-03-12 13:33:58 +0100

    update android

--- android/app/src/main/java/com/docexplain/documate/MainActivity.java
+++ android/app/src/main/java/com/docexplain/documate/MainActivity.java
@@ -2,12 +2,10 @@ package com.docexplain.documate;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
-import androidx.core.splashscreen.SplashScreen;
 
 public class MainActivity extends BridgeActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
     }
 }
